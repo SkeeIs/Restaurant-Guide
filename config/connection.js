66b6b78c41c55ef7a9584996c713cf467c0f1efa
Taylor Skeels <skeels@Taylors-MacBook-Pro.local>
2018-09-13 14:15:08 -0700

    jawsDB setup

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "restaurant_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+    
+      user: "root",
+    
+      password: "",
+      database: "restaurantsDB"
+    });
+};
 
 // Make connection.
 connection.connect(function(err) {
